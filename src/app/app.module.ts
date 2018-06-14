13189b159f936ff9cb7a9ad53779c4de5b6ebf93
Tam Vu <vu.t@husky.neu.edu>
2018-06-13 23:57:37 -0400

    render module + lessons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {CourseNavigatorServiceClient} from './services/course-navigator.service.client';
+import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
+import { WhiteBoardComponent } from './white-board/white-board.component';
+import { CourseGridComponent } from './course-grid/course-grid.component';
+import {CourseServiceClient} from './services/course.service.client';
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
+import { ModuleListComponent } from './module-list/module-list.component';
+import {ModuleServiceClient} from './services/module.service.client';
+import { LessonTabsComponent } from './lesson-tabs/lesson-tabs.component';
+import {LessonServiceClient} from './services/lesson.service.client';
+import {routing} from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseNavigatorComponent,
+    WhiteBoardComponent,
+    CourseGridComponent,
+    CourseViewerComponent,
+    ModuleListComponent,
+    LessonTabsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    CourseNavigatorServiceClient,
+    CourseServiceClient,
+    ModuleServiceClient,
+    LessonServiceClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
