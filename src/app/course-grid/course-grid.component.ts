69775cbb9b84dc22ff83040f055d825a7b513f5e
Tam Vu <vu.t@husky.neu.edu>
2018-06-20 01:32:12 -0400

    enroll in a course once only

--- src/app/course-grid/course-grid.component.ts
+++ src/app/course-grid/course-grid.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseServiceClient} from "../services/course.service.client";
 import {Course} from "../models/coruse.model.client";
+import {UserServiceClient} from '../services/user.service.client';
+import {SectionServiceClient} from '../services/section.service.client';
 
 @Component({
   selector: 'app-course-grid',
@@ -9,13 +11,31 @@ import {Course} from "../models/coruse.model.client";
 })
 export class CourseGridComponent implements OnInit {
 
-  constructor(private service: CourseServiceClient) { }
+  constructor(private service: CourseServiceClient,
+              private userService: UserServiceClient,
+              private sectionService: SectionServiceClient) { }
 
+  user = {};
   courses: Course[] = [];
-
+  sections = [];
   ngOnInit() {
+    this.userService
+      .profile()
+      .then(user =>
+        this.user = user);
+    this.sectionService
+      .findSectionsForStudent()
+      .then(sections => this.sections = sections);
     this.service.findAllCourses()
       .then(courses => this.courses = courses);
   }
-
+  checkEnrolledInCourse(courseId) {
+    console.log(this.sections);
+    for (let i = 0; i < this.sections.length; i++) {
+      if (this.sections[i].section.courseId === courseId) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
